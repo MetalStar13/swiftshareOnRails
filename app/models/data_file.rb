d7feb06f19a0d9e4034390e7157e99041cb2e1b0
Metalstar13 <metalstar1386@gmail.com>
2015-11-09 01:26:52 +0530

    new file:   Store/TaskManager.apk 	new file:   Store/pgm.txt 	modified:   app/models/data_file.rb 	modified:   log/development.log 	deleted:    tmp/pids/server.pid

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,5 +1,7 @@
 class DataFile < ActiveRecord::Base
   def self.save(fileUpload)
+	require 'digest/sha1'
+	require 'uri'
 
 =begin	
 	upload['datafile'].each do |f|
@@ -10,11 +12,22 @@ class DataFile < ActiveRecord::Base
 	end
 =end
 	# puts "Enter DataFile\n"
+	hash = Digest::MD5.hexdigest(fileUpload.join)
+	#MD5 32, SHA1 40, SHA256 64
+	
+	directory = "/home/pi/RailsServer/swiftshareOnRails/Store/" + hash
+	
+	puts "\n\n"
+	puts hash, hash.length, directory
+	puts "\n\n"
+	
+	Dir.mkdir directory
+	
 	fileUpload.each do |f|
 		name = f.original_filename
-		directory = "/home/pi/RailsServer/SwiftShare/Store/"
 		path = File.join(directory, name)
 		File.open(path, "wb") { |file| file.write(f.read) }
-	end
+	end	
+	
   end
 end
